7540ceff893836c9941dcc9d9116ecbb78357cf4
Bruno <brunojgazzo@yahoo.com.ar>
2019-11-28 17:20:37 -0300

    agrego footer y tabla con js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-// Empty JS for your own code to be here
+
+function agregar(){
+    var tabla = document.getElementById("tabla");//crea la fila de la tabla y reemplaza cell# con el valor
+    var row = tabla.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = "nombre";
+    cell2.innerHTML = "mascota";
+    cell3.innerHTML = "servicio";
+}
